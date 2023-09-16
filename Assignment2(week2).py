3fc056247c5f4e7f4e35ae7a140438d48fc2b0bb
Tanya202312 <144300965+Tanya202312@users.noreply.github.com>
2023-09-16 17:03:39 +0900

    Add files via upload

---
+++ Assignment2(week2).py
@@ -0,0 +1,7 @@
+promt=("Enter Celsius Temperature: ")
+Celsius=input(promt)
+print(type(Celsius))
+Celsius=int(Celsius)
+print(type(Celsius))
+Fahrenheit=Celsius*9/5+32
+print("Fahrenheit Temperature ", Fahrenheit)
